3a522eb9a9ad712989c7108b372029b205eb793d
wxinbang <wxinbang@outlook.com>
2022-06-07 18:00:02 +0800

    更新

--- 高考倒计时/MainPage.xaml.cs
+++ 高考倒计时/MainPage.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using Windows.ApplicationModel.Core;
+using Windows.UI;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
@@ -14,24 +19,53 @@ namespace 高考倒计时
         private string nowTimeString;
         private string leftTotalDays;
         private DateTime nowTime;
-        private readonly DateTime finalTime = new DateTime(2023, 6, 7, 9, 0, 0);
-        private readonly DateTime finalDay = new DateTime(2023, 6, 7);
+		private int nowYear;
+		private DateTime finalTime;
+		private DateTime finalDay;
         private TimeSpan leftTime;
         private TimeSpan leftDays;
         private string leftTimeString;
         private int deleteChars = 8;
         private readonly DispatcherTimer timer = new DispatcherTimer();
 
+		private List<int> years = new List<int>();
+
         public MainPage()
         {
             this.InitializeComponent();
-        }
+
+			CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+			Window.Current.SetTitleBar(AppTitleBar);
+			ApplicationViewTitleBar titleBar = ApplicationView.GetForCurrentView().TitleBar;
+			titleBar.ButtonBackgroundColor = Colors.Transparent;
+			titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+
+			nowTime = DateTime.Now;
+			nowYear = DateTime.Now.Year;
+			finalTime = new DateTime(nowYear, 6, 7, 9, 0, 0);
+			finalDay = new DateTime(nowYear, 6, 7);
+
+			if ((finalTime - nowTime).TotalMinutes >= 0) years.Add(nowYear);
+			else
+			{
+				finalTime = new DateTime(++nowYear, 6, 7, 9, 0, 0);
+				finalDay = new DateTime(nowYear, 6, 7);
+				years.Add(nowYear);
+			}
+
+			years.Add(++nowYear);
+			years.Add(++nowYear);
+
+			Years.ItemsSource = years;
+			Years.SelectedIndex = 0;
+		}
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
             timer.Interval = new TimeSpan(0, 0, 0, 0, 980);
             timer.Tick += Timer_Tick;
             timer.Start();
+			Timer_Tick(sender, e);
         }
 
         private void Timer_Tick(object sender, object e)
@@ -42,15 +76,18 @@ namespace 高考倒计时
 
             leftTime = finalTime - nowTime;
 
-            leftTimeString = leftTime.ToString();
-            leftTimeString = leftTimeString.Substring(0, leftTimeString.Length - deleteChars);
+			if (leftTime.TotalSeconds > 0)
+			{
+				leftDays = finalDay - nowTime;
+				leftTotalDays = Math.Floor(leftDays.TotalDays).ToString();
+				LeftDay.Text = "广义剩余" + (leftTotalDays == "-1" ? "0" : leftTotalDays) + "天";
+			}
+			else leftTime = new TimeSpan(0, 0, 0, 0, 0);
+			leftTimeString = leftTime.ToString();
+			leftTimeString = leftTimeString.Substring(0, leftTimeString.Length - deleteChars);
 
-            LeftTime.Text = "剩余时间:" + leftTimeString;
-
-            leftDays = finalDay - nowTime;
-            leftTotalDays = Math.Floor(leftDays.TotalDays).ToString();
-            LeftDay.Text = "广义剩余" + leftTotalDays + "天";
-        }
+			LeftTime.Text = "剩余时间:" + leftTimeString;
+		}
 
         private void FastMode_Toggled(object sender, RoutedEventArgs e)
         {
@@ -66,5 +103,12 @@ namespace 高考倒计时
                 Timer_Tick(sender, e);
             }
         }
-    }
+
+		private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			finalTime = new DateTime((int)Years.SelectedItem, 6, 7, 9, 0, 0);
+			finalDay = new DateTime((int)Years.SelectedItem, 6, 7);
+			Timer_Tick(sender, e);
+		}
+	}
 }
